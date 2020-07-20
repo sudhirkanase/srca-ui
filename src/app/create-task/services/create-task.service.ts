dce383fa07dd5e074048ea2b7174472f8717cae0
Bhushan Asolkar <bhushan.asolkar@yahoo.com>
2020-07-20 20:32:28 +0530

    update CreateTaskService to extend from BaseService

--- src/app/create-task/services/create-task.service.ts
+++ src/app/create-task/services/create-task.service.ts
@@ -4,29 +4,32 @@ import { Observable, of, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Account } from '../model/Account';
+import { BaseService } from 'src/app/services/base.service';
 
 @Injectable()
-export class CreateTaskService {
+export class CreateTaskService extends BaseService<any> {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(http: HttpClient) {
+    super(http);
+  }
 
   searchAccounts(): Observable<Account[]> {
-    return this.httpClient.get<Account[]>('assets/json/account-list.json');
+    return this.get<Account[]>('assets/json/account-list.json');
   }
 
   getAccountByAccountNumber(accountNumber: number): Observable<Account[]> {
-    return this.httpClient.get<Account[]>('assets/json/account-list.json')
+    return this.get<Account[]>('assets/json/account-list.json')
       .pipe(
         map(accounts => accounts.filter((account: Account) => account.accountNumber === accountNumber))
       );
   }
 
   getTasksByAccountNumber(accountNumber: number): Observable<any> {
-    return this.httpClient.get('assets/json/task-list.json');
+    return this.get<any>('assets/json/task-list.json');
   }
 
-  getDocumentDetails(): Observable<any>{
-    return this.httpClient.get<any>('assets/json/document-details.json')
+  getDocumentDetails(): Observable<any> {
+    return this.get<any>('assets/json/document-details.json')
   }
 
 }
