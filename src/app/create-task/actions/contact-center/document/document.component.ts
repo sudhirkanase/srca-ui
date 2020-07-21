ad1db52d4687f5914a138a2a5d49d5844fcfb7ce
pnpawar <priyankapawar059@gmail.com>
2020-07-21 18:51:17 +0530

    upload document changes

--- src/app/create-task/actions/contact-center/document/document.component.ts
+++ src/app/create-task/actions/contact-center/document/document.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { SelectItem } from 'primeng/api';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CreateTaskService } from 'src/app/create-task/services/create-task.service';
+import { Observable } from 'rxjs';
+import { UploadFileService } from 'src/app/create-task/services/UploadFileService.service';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 
 
 @Component({
@@ -18,10 +21,15 @@ export class DocumentComponent implements OnInit {
   documents: any;
   fileTypes: SelectItem[];
 
-  constructor(private documentService:CreateTaskService) { }
+  //document upload
+  selectedFiles: FileList;
+  currentFile: File;
+  progress = 0;
+  message = '';
+  constructor(private documentService:CreateTaskService,private uploadService: UploadFileService) { }
 
   ngOnInit() {
-    this.getDocumentsdetails()    
+    this.getDocumentsdetails()   
 
     this.filetypes()
 
@@ -40,7 +48,8 @@ export class DocumentComponent implements OnInit {
   documentDetailsForm = new FormGroup(
     {
       fileType : new FormControl('',Validators.required),
-      description : new FormControl('',Validators.required)
+      description : new FormControl('',Validators.required),
+      fileName : new FormControl('',Validators.required)
     }
   )
   get formAltaControls(): any {
@@ -55,10 +64,10 @@ export class DocumentComponent implements OnInit {
   filetypes(){
     this.fileTypes = [
       {label:'Miscellaneouse', value:null},
-      {label:'Text', value:{id:2, name: 'Rome', code: 'RM'}},
-      {label:'PDF', value:{id:3, name: 'London', code: 'LDN'}},
-      {label:'JPEG', value:{id:4, name: 'Istanbul', code: 'IST'}},
-      {label:'PNG', value:{id:5, name: 'Paris', code: 'PRS'}}
+      {label:'Text', value:{id:2, name: 'Text', code: 'TT'}},
+      {label:'PDF', value:{id:3, name: 'PDF', code: 'PD'}},
+      {label:'JPEG', value:{id:4, name: 'JPEG', code: 'JPG'}},
+      {label:'PNG', value:{id:5, name: 'PNG', code: 'PNG'}}
   ];
 }
 
@@ -71,8 +80,37 @@ getDocumentsdetails(){
 
 onSubmit(){
   console.log(this.documentDetailsForm.value)
+  this.upload()
 }
 onCancel(){
   this.uploadDoc = !this.uploadDoc
 }
+
+//upload file code
+selectFile(event) {
+  this.selectedFiles = event.target.files;
+}
+
+upload() {
+  this.progress = 0;
+
+  this.currentFile = this.selectedFiles.item(0);
+  this.uploadService.upload(this.currentFile).subscribe(
+    event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progress = Math.round(100 * event.loaded / event.total);
+      } else if (event instanceof HttpResponse) {
+        this.message = event.body.message;
+        console.log("uploaded")
+      }
+    },
+    err => {
+      this.progress = 0;
+      this.message = 'Could not upload the file!';
+      this.currentFile = undefined;
+    });
+
+  this.selectedFiles = undefined;
+}
+
 }
