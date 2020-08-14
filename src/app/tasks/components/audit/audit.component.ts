9ecfe8498de66d1161e855cd95bc231561c9ce0a
Heta <hetashah95@gmail.com>
2020-08-14 11:55:13 +0530

    Audit API integration

--- src/app/tasks/components/audit/audit.component.ts
+++ src/app/tasks/components/audit/audit.component.ts
@@ -3,11 +3,12 @@ import { AUDIT_COLUMNS, AUDIT_FILTER_OPTIONS } from 'src/app/app.constants';
 import { Table } from 'primeng/table';
 import { SelectItem } from 'primeng/api';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { isNullOrUndefined } from 'util';
 import { Audit } from '../../model/audit';
 import { BaseService } from '../../../services/base.service';
 import { UserInfoBean } from 'src/app/beans/userinfo-bean';
 import { formatDate } from '@angular/common';
+import { TasksService } from '../../services/tasks.service';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'srca-audit',
@@ -19,37 +20,49 @@ export class AuditComponent implements OnInit {
   auditList: any[];
   filterOptions: SelectItem[];
   addNoteForm: FormGroup;
-  display: boolean = false;
+  display = false;
   isSubmitted = false;
-  loggedInUserData:UserInfoBean;
-
+  loggedInUserData: UserInfoBean;
   @Input() auditData: any;
   @ViewChild('dt', { static: false }) table: Table;
 
-  constructor(    private baseService: BaseService
-    ) { }
+  constructor(private taskService: TasksService, private baseService: BaseService
+  ) { }
 
-  showDialog() {
-      this.display = true;
+  ngOnInit() {
+    this.initializeAddNoteForm();
+    this.columns = AUDIT_COLUMNS;
+    this.filterOptions = AUDIT_FILTER_OPTIONS;
+    if (!isNullOrUndefined(this.auditData)) {
+      this.auditList = this.auditData.audit;
+    }
   }
-  onSubmit(notes: Audit): void {
+
+  initializeAddNoteForm(): void {
+    this.addNoteForm = new FormGroup({
+      action: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit(audit: Audit): void {
     this.isSubmitted = true;
     if (this.addNoteForm.valid) {
-      const currentDate= new Date();
+      const currentDate = new Date();
       this.isSubmitted = false;
-      this.display = false;
-      this.addNoteForm.reset();
-      if (!isNullOrUndefined(notes.notes)) {
-        this.loggedInUserData=this.baseService.getLoggedInUser();
-          this.auditList.push({
-            date: formatDate(currentDate, 'MM/dd/yyyy hh:mm:ss a', 'en-US', '+0530'), 
-            user: this.loggedInUserData.username, auditType: 'Notes & Alerts',
-            action: 'Entered Note: ' + notes.notes
-          });
-      }
-      }
+      this.loggedInUserData = this.baseService.getLoggedInUser();
+      const auditRequest: Audit = {
+        taskId: this.auditData.id,
+        date: formatDate(currentDate, 'MM/dd/yyyy hh:mm:ss a', 'en-US', '+0530'),
+        user: this.loggedInUserData.username,
+        auditType: 'Notes & Alerts',
+        action: audit.action
+      };
+      this.taskService.saveAuditDetails(auditRequest).subscribe((auditData: Audit[]) => {
+        this.auditList = auditData;
+        this.display = false;
+      });
     }
-  
+  }
 
   /**
    * @description cancel method
@@ -57,39 +70,6 @@ export class AuditComponent implements OnInit {
   onCancel(): void {
     this.display = false;
     this.addNoteForm.reset();
-  }
-  initializeAddNoteForm(): void {
-    this.addNoteForm = new FormGroup({
-      notes: new FormControl(null, Validators.required),
-    });
-  }
-
-  ngOnInit() {
-    this.initializeAddNoteForm();
-    this.columns = AUDIT_COLUMNS;
-    this.filterOptions = AUDIT_FILTER_OPTIONS;
-    this.auditList = [
-      {
-        date: '08/04/2020 06:23:14 PM', user: 'SHAIKH TALAT (000224445)', auditType: 'Notes & Alerts',
-        action: 'Entered Note: Text Note'
-      },
-      {
-        date: '08/04/2020 09:20:14 PM', user: 'SHAIKH TALAT (000224445)', auditType: 'Edits',
-        action: 'Talash Shaikh assigned to account task  review maintainance summary entry'
-      },
-      {
-        date: '08/04/2020 10:03:14 PM', user: 'SHAIKH TALAT (000224445)', auditType: 'Emails',
-        action: 'Talash Shaikh assigned to account task  review maintainance summary entry'
-      },
-      {
-        date: '08/04/2020 12:00:14 PM', user: 'SHAIKH TALAT (000224445)', auditType: 'Documents',
-        action: 'Added document'
-      },
-      {
-        date: '08/04/2020 04:23:14 PM', user: 'SHAIKH TALAT (000224445)', auditType: 'Workflow',
-        action: 'Assigned to admin'
-      }
-    ];
   }
 
   onAuditTypeChange(event: any): void {
--- src/app/tasks/model/audit.ts
+++ src/app/tasks/model/audit.ts
@@ -1,10 +1,7 @@
 export class Audit {
-    auditId : number;
-    accountNumber : number;
-    taskId : number;
-    user : string;
-    auditType : string;
-    action : string;
-    date: Date;
-    notes: string;
+    taskId: number;
+    user: string;
+    auditType: string;
+    action: string;
+    date: string;
 }
