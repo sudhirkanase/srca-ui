dff1c32ece06345065e0d18a8d5f91cb466920da
Heta <hetashah95@gmail.com>
2020-09-10 20:04:46 +0530

    Role validation added

---
+++ src/app/directives/srca-role.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { SrcaRoleDirective } from './srca-role.directive';
+
+describe('Directive: SrcaRole', () => {
+  it('should create an instance', () => {
+    const directive = new SrcaRoleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
